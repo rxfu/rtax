0abbf8c93e2495810f0617d79d09162fb0f0c3e7
Stephen <stephen.frx@gmail.com>
2017-08-03 23:37:06 +0800

    add maatwebsite/excel

--- config/app.php
+++ config/app.php
@@ -183,6 +183,7 @@ return [
 		Barryvdh\Debugbar\ServiceProvider::class,
 		Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
 		Laracasts\Utilities\JavaScript\JavaScriptServiceProvider::class,
+		Maatwebsite\Excel\ExcelServiceProvider::class,
 
 	],
 
@@ -238,6 +239,7 @@ return [
 		 */
 		'Debugbar'     => Barryvdh\Debugbar\Facade::class,
 		'JavaScript'   => Laracasts\Utilities\JavaScript\JavaScriptFacade::class,
+		'Excel'        => Maatwebsite\Excel\Facades\Excel::class,
 
 	],
 
