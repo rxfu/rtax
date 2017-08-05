50892339d7f6ad31de72da461961ddc4048664be
Stephen <stephen.frx@gmail.com>
2017-08-06 00:12:23 +0800

    update footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,5 @@
 <footer>
-    <div>
+    <div class="pull-right">
         &copy; {{ date('Y') }} <a href="{{ route('home') }}">{{ config('setting.name') }}</a>. All rights reserved.<br>Powered By {{ config('setting.author') }}.
     </div>
 
