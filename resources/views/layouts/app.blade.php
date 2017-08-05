b9fc4fdf112476aba73c2a0d489e45a0db0d3877
Stephen <stephen.frx@gmail.com>
2017-08-05 16:12:09 +0800

    add html5shiv.js and respond.js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,6 +15,12 @@
     <link href="{{ asset('fonts/font-awesome/css/font-awesome.min.css') }}" rel="stylesheet">
     <link href="{{ asset('css/custom.min.css') }}" rel="stylesheet">
     @stack('styles')
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="{{ asset('html5shiv.min.js') }}"></script>
+      <script src="{{ asset('respond.min.js') }}"></script>
+    <![endif]-->
 </head>
 <body class="@yield('body_class')">
     @if (Auth::check())
