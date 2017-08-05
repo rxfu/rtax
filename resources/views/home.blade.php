39d4540ffb0f1ca94288938f82360759dc908d65
Stephen <stephen.frx@gmail.com>
2017-08-05 23:06:50 +0800

    update home template

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,7 +9,7 @@
             <div class="left_col scroll-view">
                 <div class="navbar nav_title" style="border: 0">
                     <a href="{{ route('home') }}" class="site_title">
-                        <span>{{ config('setting.name') }}</span>
+                        <i class="fa fa-paw"></i> <span>{{ config('setting.name') }}</span>
                     </a>
                 </div>
 
@@ -69,16 +69,16 @@
 
                 <!-- Menu footer buttons -->
                 <div class="sidebar-footer hidden-small">
-                    <a href="#" data-toggle="tooltip" data-placement="top" title="settings">
+                    <a href="#" data-toggle="tooltip" data-placement="top" title="设置">
                         <span class="glyphicon glyphicon-cog" aria-hidden="true"></span>
                     </a>
-                    <a href="#" data-toggle="tooltip" data-placement="top" title="FullScreen">
+                    <a href="#" data-toggle="tooltip" data-placement="top" title="全屏">
                         <span class="glyphicon glyphicon-fullscreen" aria-hidden="true"></span>
                     </a>
-                    <a href="#" data-toggle="tooltip" data-placement="top" title="Lock">
+                    <a href="#" data-toggle="tooltip" data-placement="top" title="锁定">
                         <span class="glyphicon glyphicon-eye-close" aria-hidden="true"></span>
                     </a>
-                    <a href="{{ route('logout') }}" data-toggle="tooltip" data-placement="top" title="logout">
+                    <a href="{{ route('logout') }}" data-toggle="tooltip" data-placement="top" title="登出">
                         <span class="glyphicon glyphicon-off" aria-hidden="true"></span>
                     </a>
                 </div>
@@ -91,7 +91,7 @@
             <div class="nav_menu">
                 <nav>
                     <div class="nav toggle">
-                        <a id="menu_toggle"><span class="fa fa-bars"></span></a>
+                        <a id="menu_toggle"><i class="fa fa-bars"></i></a>
                     </div>
 
                     <ul class="nav navbar-nav navbar-right">
@@ -116,42 +116,25 @@
         <!-- /Top navigation -->
 
         <!-- Page content -->
-        <div class="right-col" role="main">
-          <!-- top tiles -->
-          <div class="row tile_count">
-            <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
-              <span class="count_top"><i class="fa fa-user"></i> Total Users</span>
-              <div class="count">2500</div>
-              <span class="count_bottom"><i class="green">4% </i> From last Week</span>
+        <div class="right_col" role="main">
+            <div class="page-title">
+                <div class="title_left">
+                    <h3>Dashboard</h3>
+                </div>
             </div>
-            <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
-              <span class="count_top"><i class="fa fa-clock-o"></i> Average Time</span>
-              <div class="count">123.50</div>
-              <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>3% </i> From last Week</span>
+
+            <div class="clearfix"></div>
+
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="x_panel">
+                        <div class="x_title">
+                            <h2>Dashboard</h2>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
-              <span class="count_top"><i class="fa fa-user"></i> Total Males</span>
-              <div class="count green">2,500</div>
-              <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>34% </i> From last Week</span>
-            </div>
-            <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
-              <span class="count_top"><i class="fa fa-user"></i> Total Females</span>
-              <div class="count">4,567</div>
-              <span class="count_bottom"><i class="red"><i class="fa fa-sort-desc"></i>12% </i> From last Week</span>
-            </div>
-            <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
-              <span class="count_top"><i class="fa fa-user"></i> Total Collections</span>
-              <div class="count">2,315</div>
-              <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>34% </i> From last Week</span>
-            </div>
-            <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
-              <span class="count_top"><i class="fa fa-user"></i> Total Connections</span>
-              <div class="count">7,325</div>
-              <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>34% </i> From last Week</span>
-            </div>
-          </div>
-          <!-- /top tiles -->
-          </div>
+        </div>
         <!-- /Page content -->
     </div>
 </div>
