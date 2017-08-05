4d62edcaa1cf6df1b626bc0ce9f1670a85e8cd62
Stephen <stephen.frx@gmail.com>
2017-08-05 15:45:19 +0800

    add user seeder

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,35 +1,34 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
-        });
-    }
+class CreateUsersTable extends Migration {
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('users');
-    }
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		Schema::create('users', function (Blueprint $table) {
+			$table->increments('id');
+			$table->string('username', 50)->unique()->comment('用户名');
+			$table->string('email', 50)->unique()->comment('Email邮箱');
+			$table->string('password', 128)->comment('密码');
+			$table->string('name', 50)->comment('真实姓名');
+			$table->rememberToken();
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		Schema::dropIfExists('users');
+	}
 }
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder {
+
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run() {
+		User::create([
+			'username' => 'admin',
+			'email'    => 'admin@test.com',
+			'password' => bcrypt('123456'),
+			'name'     => '系统管理员',
+		]);
+	}
+}
