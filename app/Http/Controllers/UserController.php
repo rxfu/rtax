2abf19af272c18d319795897df9b04b348dac1cd
Stephen Fu <stephen.frx@gmail.com>
2017-08-08 23:53:08 +0800

    add user methods

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\User;
+use Illuminate\Http\Request;
 
 class UserController extends Controller {
 
@@ -10,5 +11,48 @@ class UserController extends Controller {
 		$users = User::all();
 
 		return view('user.list', compact('users'));
+	}
+
+	public function getCreate() {
+		return view('user.create');
+	}
+
+	public function postCreate(Request $request) {
+		$inputs = $request->all();
+
+		if ($request->isMethod('post')) {
+			$user = new User();
+			$user->fill($inputs);
+			$user->save();
+
+			return redirect('user.list');
+		}
+	}
+
+	public function getEdit($id) {
+		$user = User::find($id);
+
+		return view('user.edit', compact('user'));
+	}
+
+	public function postEdit(Request $request, $id) {
+		$inputs = $request->all();
+
+		if ($request->isMethod('post')) {
+			$user = User::find($id);
+			$user->fill($inputs);
+			$user->save();
+
+			return redirect('user.list');
+		}
+	}
+
+	public function postDelete(Request $request, $id) {
+		if ($request->isMethod('post')) {
+			$user = User::find($id);
+			$user->delete();
+
+			return redirect('user.list');
+		}
 	}
 }
