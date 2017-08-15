cb869c59f5af8b52cf93262b404baf6cc2b02822
Stephen <stephen.frx@gmail.com>
2017-08-15 12:38:11 +0800

    update pnotify stack in bottom center

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -14,11 +14,49 @@
     <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
     <link href="{{ asset('fonts/font-awesome/css/font-awesome.min.css') }}" rel="stylesheet">
     <link href="{{ asset('css/pnotify.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/pnotify.buttons.css') }}" rel="stylesheet">
     <link href="{{ asset('css/pnotify.nonblock.css') }}" rel="stylesheet">
     @stack('styles')
 
     <!-- Custom theme styles -->
     <link href="{{ asset('css/custom.min.css') }}" rel="stylesheet">
+    <style>
+    /* Alternate stack initial positioning. This one is done through code,
+      to show how it is done. Look down at the stack_bottomright variable
+      in the JavaScript below. */
+    .ui-pnotify.stack-bottomright {
+      /* These are just CSS default values to reset the PNotify CSS. */
+      right: auto;
+      top: auto;
+      left: auto;
+      bottom: auto;
+    }
+    .ui-pnotify.stack-custom {
+      /* Custom values have to be in pixels, because the code parses them. */
+      top: 200px;
+      left: 200px;
+      right: auto;
+    }
+    .ui-pnotify.stack-custom2 {
+      top: auto;
+      left: auto;
+      bottom: 200px;
+      right: 200px;
+    }
+    /* This one is totally different. It stacks at the top and looks
+      like a Microsoft-esque browser notice bar. */
+    .ui-pnotify.stack-bar-top {
+      right: 0;
+      top: 0;
+    }
+    .ui-pnotify.stack-bar-bottom {
+      margin-left: 15%;
+      right: auto;
+      bottom: 0;
+      top: auto;
+      left: auto;
+    }
+    </style>
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
@@ -35,6 +73,8 @@
     <script src="{{ asset('js/jquery.min.js') }}"></script>
     <script src="{{ asset('js/bootstrap.min.js') }}"></script>
     <script src="{{ asset('js/pnotify.js') }}"></script>
+    <script src="{{ asset('js/pnotify.animate.js') }}"></script>
+    <script src="{{ asset('js/pnotify.buttons.js') }}"></script>
     <script src="{{ asset('js/pnotify.nonblock.js') }}"></script>
     @stack('scripts')
 
@@ -44,29 +84,38 @@
     $(function(){
         PNotify.removeAll();
 
+        var stack_topleft = {"dir1": "down", "dir2": "right", "push": "top"};
+        var stack_bottomleft = {"dir1": "right", "dir2": "up", "push": "top"};
+        var stack_custom = {"dir1": "right", "dir2": "down"};
+        var stack_custom2 = {"dir1": "left", "dir2": "up", "push": "top"};
+        var stack_modal = {"dir1": "down", "dir2": "right", "push": "top", "modal": true, "overlay_close": true};
+        var stack_bar_top = {"dir1": "down", "dir2": "right", "push": "top", "spacing1": 0, "spacing2": 0};
+        var stack_bar_bottom = {"dir1": "up", "dir2": "right", "spacing1": 0, "spacing2": 0};
+        var stack_bottomright = {"dir1": "up", "dir2": "left", "firstpos1": 25, "firstpos2": 25};
+
         @if (session('success'))
         new PNotify({
             title: '成功',
-            text: {{ session('success') }},
+            text: '{{ session('success') }}',
             type: 'success',
             styling: 'bootstrap3',
-            addClass: 'stack-bar-top',
+            addClass: 'stack-bar-bottom',
             cornerclass: '',
-            width: '100%',
-            stack: stack_bar_top
+            width: '70%',
+            stack: stack_bar_bottom
         });
         @endif
 
         @if (session('error'))
         new PNotify({
             title: '出错啦',
-            text: {{ session('error') }},
+            text: '{{ session('error') }}',
             type: 'error',
             styling: 'bootstrap3',
-            addClass: 'stack-bar-top',
+            addClass: 'stack-bar-bottom',
             cornerclass: '',
-            width: '100%',
-            stack: stack_bar_top
+            width: '70%',
+            stack: stack_bar_bottom
         });
         @endif
 
@@ -76,10 +125,10 @@
             text: '{{ $error }}',
             type: 'error',
             styling: 'bootstrap3',
-            addclass: 'stack-bar-top',
+            addclass: 'stack-bar-bottom',
             cornerclass: '',
-            width: '100%',
-            stack: stack_bar_top
+            width: '70%',
+            stack: stack_bar_bottom
         });
         @endforeach
     });
