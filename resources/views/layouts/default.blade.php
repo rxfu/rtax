6711696d52a1bb8fb6b52d37a661ec1dbc89fba0
Stephen <stephen.frx@gmail.com>
2017-08-31 22:19:08 +0800

    update title

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -11,7 +11,7 @@
     <!-- CSRF Token -->
     <meta name='csrf-token' content='{{ csrf_token() }}'>
 
-    <title>@yield('title', 'Homepage') | {{ config('app.name', 'Laravel') }}</title>
+    <title>@yield('title', 'Homepage') | {{ config('setting.name', 'Laravel') }}</title>
 
     <!-- Styles -->
     <link href='{{ asset('css/bootstrap.min.css') }}' rel='stylesheet'>
